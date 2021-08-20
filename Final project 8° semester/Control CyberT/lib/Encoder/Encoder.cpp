97a121d4ab613f98d4a586cbd5c9f0c81bca3c56
Kingofpollos <89166477+Kingofpollos@users.noreply.github.com>
2021-08-20 15:24:09 -0500

    Update Encoder.cpp

--- Final project 8° semester/Control CyberT/lib/Encoder/Encoder.cpp
+++ Final project 8° semester/Control CyberT/lib/Encoder/Encoder.cpp
@@ -16,15 +16,15 @@ void Encoder::setup(uint8_t A, uint8_t Be, float Ang)
     _A = A;
     _Be = Be;
     _Ang = Ang;
-    pinMode(_A, INPUT);//va en el objeto
-    pinMode(_Be, INPUT);//va en el objeto
+    pinMode(_A, INPUT);
+    pinMode(_Be, INPUT);
     t_1 = micros()+15;
     t_2 = micros()+30;
 }
 
 void IRAM_ATTR Encoder::ISR() 
 {
-    //Se activa cuando hay un cambio de señal LOW-HIGH
+    //SActivates when there is a change in the signal LOW-HIGH
     t_2 = micros();
     dt = t_2 - t_1;
     t_1 = t_2;
