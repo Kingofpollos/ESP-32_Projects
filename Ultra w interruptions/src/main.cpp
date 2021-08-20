b7ef826788a08d7311e8ffdcdf311cb10e46ec14
Kingofpollos <89166477+Kingofpollos@users.noreply.github.com>
2021-08-20 13:12:09 -0500

    Update main.cpp

--- Ultra w interruptions/src/main.cpp
+++ Ultra w interruptions/src/main.cpp
@@ -9,21 +9,21 @@ ulong current_time, prev_time; //pull in de tiempo
 uint16_t dt_ms = 20;
 float dist = 0.0;
 float distf = 0.0;
-//Pasabajas
+//Low-pass filter
 float a[4] = {1.0, -1.73472577, 0.7660066, 0};
 float b[4] = {0.00782021, 0.01564042, 0.00782021, 0};
 
 HCSR OwO;
 TF filt_OwO;
 
-void IRAM_ATTR ISR_OwO() { //va en el objeto
+void IRAM_ATTR ISR_OwO() { //From the object
    OwO.ISR();
 }
 
 void setup() {
   Serial.begin(115200);
   OwO.setup(trig, echo);
-  attachInterrupt(echo, ISR_OwO , CHANGE); //no va en el objeto, isr viene del objeto
+  attachInterrupt(echo, ISR_OwO , CHANGE); 
   filt_OwO.setup(a,b,0);
   prev_time = millis();
 }
