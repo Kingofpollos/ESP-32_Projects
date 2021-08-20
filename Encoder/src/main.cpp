09211e515be110cd7f162059f1cf8cc2bf2396a7
Kingofpollos <89166477+Kingofpollos@users.noreply.github.com>
2021-08-20 13:42:08 -0500

    Update main.cpp

--- Encoder/src/main.cpp
+++ Encoder/src/main.cpp
@@ -19,38 +19,38 @@ float resolucion=3.6;
 float readEncod;
 
 //Pull in de tiempo
-ulong current_time, prev_time; //pull in de tiempo
+ulong current_time, prev_time;
 uint16_t dt_ms = 25;
 
 //PID
 PID control;
 float var_cont;
 
-//Filtro
-TF filt;//Pasabajas
+//Filter
+TF filt;//Low-Pass filter
 float a[4] = {4.0,0,0,0};
 float b[4] = {1.0,1.0,1.0,1.0};
 float readEncodFilt;
 
-//Comunicaciones
+//Comunication
 String senal;
 const int leng = 32;
 char senalc[leng];
 BluetoothSerial SerialBT;
-float control_input[2];//Tamaño de el array resultante
+float control_input[2];//Size of the resulting array
 
 
-void IRAM_ATTR ISR_uwu() { //va en el objeto
+void IRAM_ATTR ISR_uwu() {
    uwu.ISR();
 }
 
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("Bbto es grande");
+  SerialBT.begin("ESP-32");
   SerialBT.setTimeout(20);
   uwu.setup(A,B,resolucion);
-  attachInterrupt(A, ISR_uwu, RISING); //no va en el objeto, isr viene del objeto
+  attachInterrupt(A, ISR_uwu, RISING); 
   attachInterrupt(B, ISR_uwu, RISING);
   motor.setup(pinmotor1, 0, 40.0, 10);
   pinMode(pinmotor2, OUTPUT);
@@ -59,7 +59,6 @@ void setup() {
   digitalWrite(pinmotor2, LOW);
   motor.setDuty(50);
   prev_time = millis();
-  // put your setup code here, to run once:
 }
 
 void loop() {
@@ -83,5 +82,4 @@ void loop() {
     Serial.flush();
     
   }
-  // put your main code here, to run repeatedly:
 }
