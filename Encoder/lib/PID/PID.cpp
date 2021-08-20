229b124d26ea856128c56c59c1f4620762f7f79a
Kingofpollos <89166477+Kingofpollos@users.noreply.github.com>
2021-08-20 13:59:23 -0500

    Update PID.cpp

--- Encoder/lib/PID/PID.cpp
+++ Encoder/lib/PID/PID.cpp
@@ -22,7 +22,7 @@ void PID::setup(float Kp, float Ki, float Kd, uint16_t T)
 
 float PID::apply(float ref, float sens_read) 
 {
-    err = ref - sens_read; //Puede ser input o output
+    err = ref - sens_read;
     output = output_1 + err*a + err_1*b + err_2*_Kd;
     output_1 = output;
     err_2 = err_1;
@@ -32,7 +32,8 @@ float PID::apply(float ref, float sens_read)
 
 float PID::applywLim(float ref, float sens_read, float lim_inf, float lim_sup) 
 {
-    err = ref - sens_read; //Puede ser input o output
+    //This punction limits the PID to a range determined by the user
+    err = ref - sens_read; 
     output = output_1 + err*a + err_1*b + err_2*_Kd;
     if (output > lim_sup)
     {
